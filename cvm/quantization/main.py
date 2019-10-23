243d828f894dd946b988a81fc32fdda205cb0f09
Longtao Wang <longtao.wang@cortexlabs.ai>
2019-10-23 11:27:04 +0800

    update main.py

--- cvm/quantization/main.py
+++ cvm/quantization/main.py
@@ -1,4 +1,3 @@
-import json
 import sys
 from os import path
 import logging
@@ -13,97 +12,117 @@ import sym_utils as sutils
 import sim_quant_helper as sim
 import mrt as _mrt
 
-NoneCfg = object()
-class Config(object):
-    def __init__(self, name="", parent=None):
-        self.name = name
-        self.cfg = {}
-        self.parent = parent
+NoneCfg, NoneValue = object(), object()
+class Pack(object):
+    def __init__(self, name="", parent=None, dscr=""):
+        self._name, self._parent = name, parent
+        self._dscr = dscr
+        self._cfg = {}
 
-    def __getitem__(self, key):
-        return self.cfg[key]
+    def path(self):
+        if self._parent:
+            return self._parent.path() + " > " + self._name
+        return self._name
 
-    def default(self, key, default):
-        self.cfg[key] = default
+    def descripe(self, dscr):
+        self._dscr = dscr
         return self
 
-    def set_default(self, key, opt):
-        assert type(self.cfg[key]) == type(opt), \
-                "%s [%s] is invalid format, Expected %s vs. %s" \
-                % (self, key, type(self.cfg[key]).__name__, type(opt).__name__)
-        self.cfg[key] = opt
+    def item(self):
+        return self
+
+    def usage(self, count=0, ident="    "):
+        helper = "\n%s%s: %s\n" % (ident * count, self._name, self._dscr)
+        for k, v in self._cfg.items():
+            helper += v.usage(count + 1, ident)
+        return helper
+
+    def parse(self, cfg):
+        cfg = {} if cfg == NoneValue else cfg
+        for k, v in self._cfg.items():
+            val = cfg[k] if k in cfg else NoneValue
+            v.parse(val)
+
+        for k in cfg:
+            assert k in self._cfg, "%s > %s is not supported" \
+                % (self.path(), k)
+
+
+class Type(Pack):
+    def item(self):
+        return self._cfg["parsed"]
+
+    def type(self, dtype):
+        self._cfg["type"] = dtype
+        self._cfg["value"] = NoneValue
+        return self
+
+    def value(self, val):
+        self._cfg["type"] = type(val)
+        self._cfg["value"] = val
+        return self
+
+    def usage(self, count, ident):
+        helper = "%s[%-15s]: type -> %-7s" % (ident * count,
+                self._name, self._cfg["type"].__name__)
+        if self._cfg["value"] != NoneValue:
+            helper += " default -> %s" % self._cfg["value"]
+        if self._dscr:
+            helper += ", %s" % self._dscr
+        return helper + "\n"
+
+    def parse(self, cfg):
+        val = self._cfg["value"] if cfg == NoneValue else cfg
+        assert val != NoneValue, "%s is not set" % self.path()
+        assert isinstance(val, self._cfg["type"]), \
+            "%s error: invalid type, expected %s vs. %s" \
+            % (self.path(), self._cfg["type"].__name__, type(val).__name)
+        self._cfg["parsed"] = val
+
+
+class Config(Pack):
+    def __getitem__(self, key):
+        return self._cfg[key].item()
 
     def declare(self, key, dtype):
         assert isinstance(dtype, type)
-        self.cfg[key] = dtype
-        return self
+        return self._cfg.setdefault(key, Type(key, self).type(dtype))
 
-    def set_declare(self, key, opt):
-        assert isinstance(opt, self.cfg[key]), \
-                "%s [%s] is invalid format, Expected %s vs. %s" \
-                % (self, key, self.cfg[key].__name__, type(opt).__name__)
-        self.cfg[key] = opt
+    def default(self, key, default):
+        return self._cfg.setdefault(key, Type(key, self).value(default))
 
     def config(self, key):
-        self.cfg[key] = Config(key, self)
-        return self.cfg[key]
+        return self._cfg.setdefault(key, Config(key, self))
 
-    def set_config(self, key, cfg):
-        self.cfg[key].parse(cfg)
+cfg = Config("Configuration").descripe("MRT Json Format")
+cfg.declare("symbol", str).descripe("symbol file path")
+cfg.declare("params", str).descripe("params file path")
+cfg.declare("input_shape", tuple)
+cfg.declare("dataset", str).descripe( \
+        "optional ['imagenet', 'voc', 'mnist', 'quickdraw']")
 
-    def __str__(self):
-        if self.parent:
-            return str(self.parent) + " > " + self.name
-        return self.name
+_cfg = cfg.config("quantization").descripe("Config For Quantization")
+_cfg.declare("batch_size", int)
+_cfg.default("pure_int8", False)
+_cfg.default("calibrate_num", 1)
+_cfg.default("device", "cpu:0").descripe( \
+        "(format) device:number optional ['cpu', 'gpu']")
+_cfg.declare("output_precision", int)
+_cfg.default("fixed", [])
+_cfg.default("thresholds", {})
+_cfg.default("split_names", [])
+_cfg.default("name_maps", {})
+_cfg.default("attr_scales", {})
+_cfg.default("log", False)
 
-    def parse(self, cfg):
-        for k, v in self.cfg.items():
-            if isinstance(v, type):
-                assert k in cfg, "%s [%s] is not set" % (self, k)
-                self.set_declare(k, cfg[k])
-            elif isinstance(v, Config):
-                assert k in cfg, "%s [%s] is not set" % (self, k)
-                self.set_config(k, cfg[k])
-            elif k in cfg:
-                self.set_default(k, cfg[k])
+_cfg = cfg.config("cvm").descripe("Config For Compiling to CVM")
+_cfg.default("batch_size", -1).descripe( \
+        "-1 indicates batch in section `quantization`")
+_cfg.default("save_ext", False)
+_cfg.default("dir", "./")
 
-        for k in cfg:
-            assert k in self.cfg, "%s [%s] is not supported" % (self, k)
-
-config = (Config()
-    .declare("symbol", str)
-    .declare("params", str)
-    .declare("input_shape", tuple)
-    .declare("dataset", str)
-)
-
-(config.config("quantization")
-    .declare("batch_size", int)
-    .default("pure_int8", False)
-    .default("calibrate_num", 1)
-    .default("device", "cpu:0")
-    .declare("output_precision", int)
-
-    .default("fixed", [])
-    .default("thresholds", {})
-
-    .default("split_names", [])
-    .default("name_maps", {})
-    .default("attr_scales", {})
-
-    .default("log", False)
-)
-
-(config.config("cvm")
-    .default("batch_size", -1)
-    .default("save_ext", False)
-
-    .default("dir", "./")
-)
-
-(config.config("accuracy")
-    .default("iter_num", 0)
-)
+_cfg = cfg.config("accuracy").descripe("Config For Accuracy Evalulate")
+_cfg.default("iter_num", 0)
 
 if __name__ == "__main__":
     utils.log_init()
@@ -117,16 +136,17 @@ if __name__ == "__main__":
         lines = [l.strip() for l in fin.readlines()]
         lines = [l for l in lines if not l.startswith("#")]
         lines = [l for l in lines if not l == ""]
-        cfg = eval(" ".join(lines))
+        config = eval(" ".join(lines))
 
-    config.parse(cfg)
-    sym_file, prm_file = config["symbol"], config["params"]
+    cfg.parse(config)
+
+    sym_file, prm_file = cfg["symbol"], cfg["params"]
     if not path.isabs(sym_file):
         sym_file = path.abspath(path.join(baseDir, sym_file))
     if not path.isabs(prm_file):
         prm_file = path.abspath(path.join(baseDir, prm_file))
 
-    qconf = config["quantization"]
+    qconf = cfg["quantization"]
     batch_size = qconf["batch_size"]
     pure_int8 = qconf["pure_int8"]
     calibrate_num = qconf["calibrate_num"]
@@ -134,13 +154,13 @@ if __name__ == "__main__":
     device = qconf["device"].split(":")
     ctx = mx.gpu(int(device[1])) if device[0] == "gpu" else mx.cpu()
 
-    input_shape = config["input_shape"]
+    input_shape = cfg["input_shape"]
     shp = tuple(batch_size if s == -1 else s for s in input_shape)
     inputs_ext = { "data": {
         "shape": shp,
     } }
 
-    dataset = config["dataset"]
+    dataset = cfg["dataset"]
     if dataset == "imagenet":
         data_iter = ds.load_imagenet_rec(batch_size, shp[2])
         def data_iter_func():
@@ -239,17 +259,18 @@ if __name__ == "__main__":
         qsym, qparams = _mrt.merge_model(qsym, qparams, sym2, prm2, maps, op_scales)
 
     cvm_flag = cfg["cvm"]
-    cvm_batch_size = cvm_flag.get("batch_size", batch_size)
+    cvm_batch_size = cvm_flag["batch_size"]
+    cvm_batch_size = batch_size if cvm_batch_size == -1 else cvm_batch_size
 
     shp = tuple(cvm_batch_size if s == -1 else s for s in input_shape)
     inputs_ext["data"]["shape"] = shp
     nnvm_sym, nnvm_params = spass.mxnet_to_nnvm(qsym, qparams, inputs_ext)
 
-    cvm_dir = config["cvm"]["dir"]
+    cvm_dir = cfg["cvm"]["dir"]
     spass.cvm_build(nnvm_sym, nnvm_params, inputs_ext,
             path.join(cvm_dir, "cvm.symbol"),
             path.join(cvm_dir, "cvm.params"))
 
-    if config["cvm"]["save_ext"]:
+    if cfg["cvm"]["save_ext"]:
         sim.save_ext(path.join(cvm_dir, "cvm.ext"), inputs_ext, oscales)
 
