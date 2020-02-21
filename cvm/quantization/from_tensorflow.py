9d3ee5a924462b12f3f280c2aeadd9f21637f8df
Ruoyang Tang <tang_1995@126.com>
2020-02-21 16:37:42 +0800

    convert tflite

--- cvm/quantization/from_tensorflow.py
+++ cvm/quantization/from_tensorflow.py
@@ -19,6 +19,9 @@ from tfm_pass import infer_shape
 import sym_utils as sutils
 import sym_pass as spass
 
+def _queue_dequeue(inputs, attrs, params):
+    return inputs[0]
+
 def _argmax(inputs, attrs, params):
     cname = inputs[1].attr('name')
     axis = params[cname].asnumpy()
@@ -152,10 +155,6 @@ def _conv2d(opname):
             dilated_kernel_w = (kernel_w - 1) * dilation_w + 1
             pad_v = _get_pad_pair(in_h, dilated_kernel_h, stride_h)
             pad_h = _get_pad_pair(in_w, dilated_kernel_w, stride_w)
-            #  if pad_v[0] == pad_v[1] and pad_h[0] == pad_h[1]:
-                #  padding = (pad_v[0], pad_h[0])
-            #  else:
-                #  padding = (pad_v[0], pad_v[1], pad_h[0], pad_h[1])
             # TODO(wlt): mxnet not supported four-dimension padding
             # padding = (max(pad_v), max(pad_h))
             # assert pad_v[0] == pad_v[1], pad_v
@@ -212,6 +211,11 @@ def _pool2d(pool_type):
             data_shp = [data_shp[ii] for ii in (0, 3, 1, 2)]
 
         H_idx, W_idx = data_format.find("H"), data_format.find("W")
+        dilations = attrs.get('dilations', (1, 1))
+        if isinstance(dilations, int):
+            dilations = (dilations, dilations)
+        elif len(dilations) == 4:
+            dilations = (dilations[H_idx], dilations[W_idx])
         strides = attrs.get('strides')
         if isinstance(strides, int):
             strides = (strides, strides)
@@ -231,15 +235,17 @@ def _pool2d(pool_type):
             stride_h, stride_w = strides
             kernel_h, kernel_w = kernel_size
             in_h, in_w = data_shp[2], data_shp[3]
-
-            pad_v = _get_pad_pair(in_h, kernel_h, stride_h)
-            pad_h = _get_pad_pair(in_w, kernel_w, stride_w)
+            dilation_h, dilation_w = dilations
+            dilated_kernel_h = (kernel_h - 1) * dilation_h + 1
+            dilated_kernel_w = (kernel_w - 1) * dilation_w + 1
+            pad_v = _get_pad_pair(in_h, dilated_kernel_h, stride_h)
+            pad_h = _get_pad_pair(in_w, dilated_kernel_w, stride_w)
             # assert pad_v[0] == pad_v[1]
             # assert pad_h[0] == pad_h[1]
             # padding = (pad_v[0], pad_h[0])
             if pad_v[0] != pad_v[1] or pad_h[0] != pad_h[1]:
                 node = mx.sym.pad(
-                    inputs[0], mode="constant", constant_value=0,
+                    inputs[0], mode="edge",
                     pad_width=(0, 0, 0, 0, pad_v[0], pad_v[1], pad_h[0], pad_h[1]))
                 inputs[0] = node
                 padding = (0, 0)
@@ -468,6 +474,7 @@ def _squeeze(inputs, attrs, params):
 
 
 _convert_map = {
+    'QueueDequeueManyV2'                : _queue_dequeue,
     'ArgMax'                            : _argmax,
     'Add'                               : _elemwise('elemwise_add'),
     'AvgPool'                           : _pool2d("avg"),
@@ -579,6 +586,7 @@ def convert_operator(op_name, inputs, attrs, params, logger=logging):
     return sym
 
 currSupportedOps = {
+                       'FIFOQueueV2', 'QueueDequeueManyV2',
                        'ArgMax',
                        'Const',
                        'Pad',
@@ -599,6 +607,12 @@ currSupportedOps = {
                    }
 
 import tensor_util
+
+from tensorflow.core.framework import types_pb2
+default_input_shape = [1, 224, 224, 3]
+default_tf_dtype = types_pb2.DT_FLOAT
+default_tf_start_types = {'Placeholder', 'PlaceholderWithDefault', 'FIFOQueueV2'}
+
 def convert_tfnode(tfnode, graph, params, infer_shapes, logger=logging):
     name, op_name = tfnode.name, tfnode.op
     attr, org_inputs = tfnode.attr, tfnode.input
@@ -619,10 +633,15 @@ def convert_tfnode(tfnode, graph, params, infer_shapes, logger=logging):
             elif k not in ('dtype', '_output_shapes', '_class'):
                 raise NotImplementedError \
                     ("Other attributes for a Const(param) Node {} ? .".format(k))
-    elif op_name in ['Placeholder', 'PlaceholderWithDefault']:
+    elif op_name in ['Placeholder', 'PlaceholderWithDefault', 'FIFOQueueV2']:
         input_shape = \
             tensor_util.tensor_shape_proto_to_list(attr['shape'].shape)
-        dtype = tensor_util.tensor_type_to_numpy(attr['dtype'].type)
+        if not input_shape:
+            input_shape = default_input_shape
+        try:
+            dtype = tensor_util.tensor_type_to_numpy(attr['dtype'].type)
+        except TypeError:
+            dtype = tensor_util.tensor_type_to_numpy(default_tf_dtype)
         graph[name] = [mx.sym.var("data", shape=input_shape, dtype=dtype)]
         assert "data" not in infer_shapes
         infer_shapes["data"] = [tuple(input_shape)]
@@ -702,6 +721,9 @@ def convert_model(pbfile, layout="NHWC", outputs=None):
     logger.info("Operators successfully converted.")
 
     # The last node of tfgraph is assumed as output by default.
+    # Ambiguity occurs in the model `densenet_lite` since multiple outputs exist
+    # Thus, we'd better specify the outputs for the model with multiple outputs
+    # Fill in the variable `outputs_list`
     if outputs is None:
         outputs = [tfgraph.node[-1].name]
     nodes = []
@@ -784,7 +806,6 @@ def _fuse_pad(sym, params):
 
         return ret, params
 
-
     sym, params = sutils.topo_visit_transformer(sym, params,
             _fuse_custom_pad_transpose, infer_shapes=infer_shapes)
     sym, params = sutils.topo_visit_transformer(sym, params, _fuse_custom_pad,
@@ -792,6 +813,29 @@ def _fuse_pad(sym, params):
     # sym, params = sutils.topo_visit_transformer(sym, params, _fuse_pad_eq,
             # infer_shapes=infer_shapes)
     return sym, params
+
+model_outs_revise = {
+    "densenet_lite",
+    "inception_v3_lite",
+    "mobilenet_v1_0.25_128_lite",
+    "mobilenet_v1_0.25_224_lite",
+    "mobilenet_v1_0.50_128_lite",
+    "mobilenet_v1_0.50_192_lite",
+    "mobilenet_v1_1.0_224_lite",
+    "mobilenet_v2_1.0_224_lite",
+    "resnet_v2_lite",
+}
+
+def _revise_output(modelname, symbol, params):
+    if modelname not in model_outs_revise:
+        return symbol, params
+    # single outputs is assumed
+    symbol = mx.sym.slice_axis(symbol, axis=1, begin=1, end=None)
+    symbol = mx.sym.argmax(symbol, axis=1)
+    # name = "revise_outs"
+    # symbol = mx.sym.broadcast_sub(symbol, mx.sym.var(name))
+    # params[name] = mx.nd.array([1])
+    return symbol, params
 
 def dump(model, symbol, params):
     logger = logging.getLogger('model dump')
@@ -811,35 +855,50 @@ def dump(model, symbol, params):
     nd.save(params_file, params)
     logger.info("Model successfully dumped to '%s'", sym_file)
 
-def tf_dump_model(modelname):
+def tf_dump_model(modelname, revise_outs=True):
     utils.log_init()
     model_path = modelfile[modelname]
-    sym, params = convert_model(model_path)
+    outputs = outputs_list[modelname]
+    sym, params = convert_model(model_path, outputs=outputs)
     sym, params = _fuse_pad(sym, params)
+    if revise_outs:
+        sym, params = _revise_output(modelname, sym, params)
     dump(modelname, sym, params)
 
 modelfile = {
-                # "resnet50_v1": "/data/tfmodels/resnet50_v1_new/model.pb",
-                # "inception_v3": "/data/tfmodels/inception_v3/model.pb",
-                # "mobilenet": "/data/tfmodels/mobilenet/model.pb",
-                # "densenet_lite": "/data/tfmodels/lite/DenseNet/densenet.pb",
+                "resnet50_v1_new": "/data/tfmodels/resnet50_v1_new/model.pb",
+                "inception_v3": "/data/tfmodels/inception_v3/model.pb",
+                "mobilenet": "/data/tfmodels/mobilenet/model.pb",
+                "densenet_lite": "/data/tfmodels/lite/DenseNet/densenet.pb",
                 "inception_v3_lite": "/data/tfmodels/lite/Inception_V3/inception_v3.pb",
-                # "mobilenet_v1_0.25_128_lite": "/data/tfmodels/lite/Mobilenet_V1_0.25_128/mobilenet_v1_0.25_128_frozen.pb",
-                # "mobilenet_v1_0.25_224_lite": "/data/tfmodels/lite/Mobilenet_V1_0.25_224/mobilenet_v1_0.25_224_frozen.pb",
-                # "mobilenet_v1_0.50_128_lite": "/data/tfmodels/lite/Mobilenet_V1_0.50_128/mobilenet_v1_0.5_128_frozen.pb",
-                # "mobilenet_v1_0.50_192_lite": "/data/tfmodels/lite/Mobilenet_V1_0.50_192/mobilenet_v1_0.5_192_frozen.pb",
-                # "mobilenet_v1_1.0_224_lite": "/data/tfmodels/lite/Mobilenet_V1_1.0_224/mobilenet_v1_1.0_224_frozen.pb",
-                # "mobilenet_v2_1.0_224_lite": "/data/tfmodels/lite/Mobilenet_V2_1.0_224/mobilenet_v2_1.0_224_frozen.pb",
-                # "resnet_v2_lite": "/data/tfmodels/lite/ResNet_V2_101/resnet_v2_101_299_frozen.pb",
+                "mobilenet_v1_0.25_128_lite": "/data/tfmodels/lite/Mobilenet_V1_0.25_128/mobilenet_v1_0.25_128_frozen.pb",
+                "mobilenet_v1_0.25_224_lite": "/data/tfmodels/lite/Mobilenet_V1_0.25_224/mobilenet_v1_0.25_224_frozen.pb",
+                "mobilenet_v1_0.50_128_lite": "/data/tfmodels/lite/Mobilenet_V1_0.50_128/mobilenet_v1_0.5_128_frozen.pb",
+                "mobilenet_v1_0.50_192_lite": "/data/tfmodels/lite/Mobilenet_V1_0.50_192/mobilenet_v1_0.5_192_frozen.pb",
+                "mobilenet_v1_1.0_224_lite": "/data/tfmodels/lite/Mobilenet_V1_1.0_224/mobilenet_v1_1.0_224_frozen.pb",
+                "mobilenet_v2_1.0_224_lite": "/data/tfmodels/lite/Mobilenet_V2_1.0_224/mobilenet_v2_1.0_224_frozen.pb",
+                "resnet_v2_lite": "/data/tfmodels/lite/ResNet_V2_101/resnet_v2_101_299_frozen.pb",
             }
+
+outputs_list = {
+    "resnet50_v1_new": ["fc1000_1/Softmax"],
+    "inception_v3": ["predictions_1/Softmax"],
+    "mobilenet": ['act_softmax_1/Softmax'],
+    "densenet_lite": ["softmax_tensor"],
+    # "densenet_lite": ["ArgMax"],
+    "inception_v3_lite": None,
+    "mobilenet_v1_0.25_128_lite": None,
+    "mobilenet_v1_0.25_224_lite": None,
+    "mobilenet_v1_0.50_128_lite": None,
+    "mobilenet_v1_0.50_192_lite": None,
+    "mobilenet_v1_1.0_224_lite": None,
+    "mobilenet_v2_1.0_224_lite": ["MobilenetV2/Predictions/Reshape_1"],
+    "resnet_v2_lite": None,
+}
 
 if __name__ == '__main__':
     utils.log_init()
-    modelname = 'densenet_lite'
-    outputs = ['ArgMax', 'softmax_tensor']
-    modelname = "inception_v3_lite"
-    outputs = None
-    model_path = modelfile[modelname]
-    sym, params = convert_model(model_path, outputs=outputs)
-    dump(modelname, sym, params)
+    assert len(sys.argv) == 2, "Please enter 2 python arguments."
+    modelname = sys.argv[1]
+    tf_dump_model(modelname)
 
